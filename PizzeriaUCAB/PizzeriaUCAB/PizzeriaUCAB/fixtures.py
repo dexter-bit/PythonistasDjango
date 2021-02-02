3c8a711e82869926eb68e3a24bfa1075ad271412
angelsucasas <angel.alejandro.sucasas08@gmail.com>
2021-02-02 12:24:03 -0400

    reestructurando el proyecto

---
+++ PizzeriaUCAB/PizzeriaUCAB/PizzeriaUCAB/fixtures.py
@@ -0,0 +1,58 @@
+[
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 1,
+    "fields": {
+      "name": "jamon",
+      "price": 10
+    }
+  },
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 2,
+    "fields": {
+      "name": "maiz",
+      "price": 30
+    }
+  },
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 3,
+    "fields": {
+      "name": "queso",
+      "price": 30
+    }
+  }
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 4,
+    "fields": {
+      "name": "champiñon",
+      "price": 30
+    }
+  },
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 5,
+    "fields": {
+      "name": "aceite",
+      "price": 30
+    }
+  },
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 6,
+    "fields": {
+      "name": "peperoni",
+      "price": 30
+    }
+  },
+  {
+    "model": "web_pizzeria.Ingredient",
+    "pk": 7,
+    "fields": {
+      "name": "salami",
+      "price": 30
+    }
+  }
+]
---
+++ PizzeriaUCAB/PizzeriaUCAB/PizzeriaUCAB/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Pizza, PizzaIngredient, Ingredient, PizzaOrder, Client
+
+class PizzaForm(forms.Form):
+	class Meta:
+		model =Ingredient,
+		fields=[
+			'name',
+			'price'
+		]
+	shipping_address = forms.CharField(required=False)
+	shipping_address2 = forms.CharField(required=False)
