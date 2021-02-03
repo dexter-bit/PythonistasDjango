2d7c2ab6f896b9684286fd35869e3a658f21b05f
jarjar721 <jarp820@gmail.com>
2021-02-03 17:45:44 -0400

    initial commiit

---
+++ PizzeriaUCAB/PizzeriaUCAB/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import (
+	Ingredient,
+	Pizza,
+	Order
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ingredient/', Ingredient.as_view(), name='ingredient'),
+    path('pizza/', Pizza.as_view(), name='pizza'),
+    path('order/', Order.as_view(), name='order'),
+
+    path('', include('store.urls')),
+]
